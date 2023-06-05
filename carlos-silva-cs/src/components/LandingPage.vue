a81627aa31974bb2b1bd99a2fac422ed6211e886
vitoria-a <vitoriagomes@alu.ufc.br>
2023-06-05 20:18:52 -0300

    Adding redirect buttons for social network (Instagram).

--- carlos-silva-cs/src/components/LandingPage.vue
+++ carlos-silva-cs/src/components/LandingPage.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
+  <div class="landing-page">
     <header class="header">
       <img src="../assets/images/carlos-silva-perfil.jpg">
       <h1> Carlos Silva </h1>
+      <h3> Bem-vindos ao mundo CS ✅ </h3>
     </header>
 
     <main class="main">
-      <button> Click me </button>
-      <button> Click me </button>
-      <button> Click me </button>
+      
+        <a class="neon-white" href="https://www.instagram.com/centrodetreinamento.cs/">
+          🏋️ Centro de Treinamento CS
+        </a>
+
+        <a class="neon-white" href="https://www.instagram.com/cs.powernutri/">
+          💊 CS Power Nutri - Suplementos e Moda Fitness
+        </a>
+
+        <a class="neon-white" href="https://www.instagram.com/cs.shoes1/">
+          👟 CS Shoes - Tênis de qualidade você encontra aqui
+        </a>
     </main>
   </div>
 </template>
@@ -18,7 +28,3 @@ export default {
   name: "LandingPage"
 }
 </script>
-
-<style>
-
-</style>
